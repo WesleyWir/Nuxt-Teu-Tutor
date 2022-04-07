8350763be8ae1589311502ae2c4e5bee09fa1004
Wesley Wirmond <wesleywirmond@gmail.com>
2022-04-07 01:52:55 -0300

    structure internal

---
+++ pages/students/in/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Perfil aqui</h1>
+</template>
+
+<script>
+export default {
+  middleware: 'auth'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/students/in/edit/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Edição de Perfil aqui</h1>
+</template>
+
+<script>
+export default {
+  middleware: 'auth'
+
+}
+</script>
+
+<style>
+
+</style>
