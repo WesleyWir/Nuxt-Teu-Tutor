73b25d0f87669bdfd1f6c8ec7e4dc63290af42d3
Wesley Wirmond <wesleywirmond@gmail.com>
2022-06-10 23:25:50 -0300

    skull

---
+++ pages/educators/_id/checkout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="checkout" class="container">
+    <div class="form-group">
+      <label for="exampleFormControlTextarea1">Nota:</label>
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        placeholder="Descreva aqui a forma como o educador pode lhe contatar e outras anotações referente as aulas..."
+        rows="3"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
