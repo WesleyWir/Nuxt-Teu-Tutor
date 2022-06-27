5f42d889cc7997fe00f94e92c414a762283860bb
Wesley Wirmond <wesleywirmond@gmail.com>
2022-06-26 23:11:26 -0300

    in progress..

--- pages/educators/in/classes/index.vue
+++ pages/educators/in/classes/index.vue
@@ -4,19 +4,24 @@
       <h2 class="fw-bold">Minhas Aulas</h2>
     </div>
     <div id="classes-items">
-      <div class="item">
+      <div class="item" v-for="cl in classes" :key="cl.id">
         <div
           class="student-info d-flex justify-content-center align-items-center"
         >
           <div class="student-image">
             <img
-              :src="'/imgs/user/default-profile.png'"
-              alt="Profile Image"
-              class="rounded-circle w-25"
+              :src="
+                getImageFromBackend(
+                  cl.student.avatar,
+                  '/imgs/user/default-profile.png'
+                )
+              "
+              :alt="cl.student.name"
+              class="rounded-circle w-50"
             />
           </div>
           <div class="student-name">
-            <h3>Lorem Ipsum</h3>
+            <h3>{{ cl.student.name }}</h3>
           </div>
         </div>
         <div class="class-calendars">
@@ -42,22 +47,30 @@
             <div class="note">
               <div class="form-group" id="checkout-note">
                 <label for="note">Nota:</label>
-                <textarea
-                  class="form-control"
-                  id="note"
-                  rows="3"
-                ></textarea>
+                <textarea class="form-control" id="note" rows="3"></textarea>
               </div>
             </div>
           </div>
         </div>
+        <div class="class-note d-flec justify-content-center">{{ cl.note }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios }) {
+    try {
+      const { data } = await $axios.get("/classes/educators/");
+      console.log(data);
+      const classes = data;
+      return { classes };
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
 </script>
 
 <style>
